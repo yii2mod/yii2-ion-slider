beef5b34a5f644082078e7cd9850d495d119bb69
Igor Chepurnoy <igorzfort@gmail.com>
2015-02-16 11:01:08 +0200

    Update IonSlider.php

--- IonSlider.php
+++ IonSlider.php
@@ -48,6 +48,8 @@ class IonSlider extends InputWidget
     {
         if ($this->hasModel()) {
             echo Html::activeTextInput($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textInput($this->name, $this->value, $this->options);
         }
         $this->registerAssets();
     }
